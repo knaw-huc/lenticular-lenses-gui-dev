e1ab8e5e40615130f093aebf2ac82152671eb425
Rob Zeeman <this_is_not_an_email-3cc036843bde09c86580da2d3d753a527d1e8bfa@authentication.clariah.nl>
2020-09-08 10:47:04 +0200

    Build files

---
+++ www/precache-manifest.344ffe7de90d28ce48268ab39adfcc49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c545ed00c25c4f2729e432b4d968b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62978790df969a75b1de",
+    "url": "/static/css/main.c7fe3770.chunk.css"
+  },
+  {
+    "revision": "a00591ede2f9dfd18dc2",
+    "url": "/static/js/2.4a9b5ba2.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.4a9b5ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62978790df969a75b1de",
+    "url": "/static/js/main.9cf45239.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  },
+  {
+    "revision": "69311753847c9c5a12850f856f77d82f",
+    "url": "/static/media/logo-ga.69311753.png"
+  }
+]);
